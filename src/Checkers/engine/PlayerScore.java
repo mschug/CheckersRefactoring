a20eb0232dd9cbed6a954ed61bf1bc94c6049970
mschug <mss9627@rit.edu>
2015-05-04 13:03:05 -0400

    File opening and saving has been implemented. PlayerScore has been given a toString() which produces a valid CSV line.

--- src/Checkers/engine/PlayerScore.java
+++ src/Checkers/engine/PlayerScore.java
@@ -6,10 +6,18 @@ package Checkers.engine;
  * @author mss9627
  */
 public class PlayerScore implements Comparable {
-    private String name;
+    private final String name;
     private int wins;
     private int games;
     
+    /**
+     * Creates a new PlayerScore object, and confirms that the win rate
+     * for this player can be valid.
+     * 
+     * @param playerName The player's name.
+     * @param numWins    The total number of wins for this player.
+     * @param numGames   The combined wins, losses, and draws for this player.
+     */
     public PlayerScore( String playerName, int numWins, int numGames ){
         name = playerName;
         wins = numWins;
@@ -17,55 +25,57 @@ public class PlayerScore implements Comparable {
         checkValidity();
     }
     
-    /*
+    /**
      * Checks whether a user has made a manual, invalid change to the score file.
      * If a record has a win percentage greater than 100%, reset both the wins 
      * and games played to 0.
      */
-    private boolean checkValidity(){
+    private void checkValidity(){
+        if( games < 0 ) games = 0;
+        if( wins < 0 ) wins = 0;
+        
         if( games < wins ){
             System.out.println( "Games played for " + name + " are invalid.");
             System.out.println( "The record for " + name + "has been reset.");
             games = 0;
             wins = 0;
-            return false;
         }
-        return true;
     }
     
-    /*
+    /**
      * Updates a player's record after a game is completed.
+     *
+     * @param hasWon True for a player winning a game.
      */
     public void addGame( boolean hasWon ){
         games++;
         if( hasWon ) wins++;
     }
     
-    /*
+    /**
      * Returns the percentage of won games for the given player.
      * A player with no games played is considered to have a 0% win rate.
+     * 
+     * @return the percentage of wins to total games out of 100%
      */
     public double getWinPercentage(){
         if( games == 0 ) return 0;
-        return ((double) wins) / games;
+        return ((double) wins / games) * 100;
     }
     
-    public String getName(){
-        return name;
-    }
-    
-    public int getWins(){
-        return wins;
-    }
-    
-    public int getGames(){
-        return games;
-    }
+    public String getName(){ return name; }
+    public int getWins(){ return wins; }
+    public int getGames(){ return games; }
 
-    /*
+    /**
      * Used to sort a list of players by name.
      * This is required to add new players in sorted order.
-     */
+     *
+     * @param p The PlayerScore being compared with this object.
+     * @return positive if this player's name is lexicographically ahead of the 
+     *         other player's name, negative if lexicographically behind, or 
+     *         zero if equal.
+     */    
     @Override
     public int compareTo( Object p ) {
         if( p instanceof PlayerScore ){
@@ -73,6 +83,17 @@ public class PlayerScore implements Comparable {
             return name.compareTo( temp.getName() );
         }
         throw new ClassCastException( "Object must be of type PlayerScore." );
+    }
+    
+    /**
+     * Creates a comma separated string for the PlayerScore to place in the CSV file.
+     * @return a string in the format name,wins,games
+     */
+    @Override
+    public String toString(){
+        String winNum = Integer.toString(wins);
+        String gameNum = Integer.toString(games);
+        return name + "," + winNum + "," + gameNum;
     }
     
 }
--- src/Checkers/engine/ScoreboardController.java
+++ src/Checkers/engine/ScoreboardController.java
@@ -17,9 +17,10 @@ import java.util.ArrayList;
 public class ScoreboardController {
     public static final boolean WIN = true;
     public static final boolean LOSE = false;
+    public static final boolean DRAW = false;
     private BufferedReader fileIn;
     private BufferedWriter fileOut;
-    private ArrayList players;
+    private ArrayList<PlayerScore> players;
     
     public ScoreboardController(){
         try {
@@ -30,11 +31,68 @@ public class ScoreboardController {
                 FileOutputStream out = new FileOutputStream("scoreboard.csv");
                 out.close();
                 fileIn = new BufferedReader( new FileReader("scoreboard.csv") );
-            } catch( IOException io ){
-                io.printStackTrace();
-            }
+            } catch( IOException io ){}
         }
         
-        players = new ArrayList<PlayerScore>();
+        players = new ArrayList<>();
+        try {
+            while( fileIn.ready() ){
+                parseLine( fileIn.readLine() );
+            }
+            fileIn.close();
+        } catch( IOException e ){}
     }
+    
+    /**
+     * Processes each input line, creating PlayerScore objects for each line.
+     * Removes invalid input lines from the scoreboard.
+     * 
+     * @param line The next input line to read.
+     */
+    private void parseLine( String line ){
+        String[] params = line.split(",");
+        
+        if( params.length != 3 ){
+            System.out.println( "Input line was invalid." );
+            System.out.println( "Line \"" + line + "\" will be erased upon the next save.");
+        }
+        
+        String name = params[0];
+        int wins = tryParse( params[1] );
+        int games = tryParse( params[2] );
+        
+        PlayerScore p = new PlayerScore(name, wins, games);
+        players.add( p );
+    }
+    
+    /*
+     * Attempts to parse an integer from a line in the CSV file.
+     * 
+     * @param num The string of (what should be) an integer.
+     * @return the value of num if valid, or 0 if invalid.
+     */
+    private int tryParse( String num ){
+        try {
+            return Integer.parseInt( num );
+        } catch( NumberFormatException e ){
+            return 0;
+        }
+    }
+    
+    /**
+     * Overwrites the CSV file, saving new records from the players list.
+     * Any invalid lines found while opening the file are erased.
+     */
+    private void saveFile(){
+        try {
+            fileOut = new BufferedWriter( new FileWriter("scoreboard.csv") );
+            for( PlayerScore p : players ){
+                fileOut.write( p.toString() );
+                fileOut.newLine();
+            }
+            fileOut.close();
+        } catch( FileNotFoundException f ){
+        } catch( IOException io ){}
+    }
+    
 }
